db77b88fc3724593f726ac227dda8f19331d20f4
Ryan Freeman <info@ryanfreeman.io>
2019-10-14 00:27:03 +0100

    Added styling to todo alert

--- public/js/app.js
+++ public/js/app.js
@@ -36986,8 +36986,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/ryan/Google Drive/devilbox/data/www/todo/todo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/ryan/Google Drive/devilbox/data/www/todo/todo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /shared/httpd/todo/todo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /shared/httpd/todo/todo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
