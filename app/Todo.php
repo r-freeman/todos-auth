90559f14d6c819bcfd7bb930fa910fb1eb2bae00
Ryan Freeman <info@ryanfreeman.io>
2019-10-15 01:48:52 +0100

    Added one to many inverse relationship on todos/user, updated todo index template

--- app/Todo.php
+++ app/Todo.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -5,10 +5,12 @@
     <ul class="list-group py-3 mb-3">
         @forelse($todos as $todo)
             <li class="list-group-item my-2">
-                <h5>{{ $todo->title }}</h5>
-                <p>{{ Str::limit($todo->body,10) }}</p>
+                <h5 class="float-left">{{ $todo->title }}</h5>
+                <p class="float-right">Created by: {{ $todo->user->name }}</p> {{-- from the user relationship --}}
+                <div class="clearfix"></div>
+                <p>{{ Str::limit($todo->body,20) }}</p>
                 <small class="float-right">{{ $todo->created_at->diffForHumans() }}</small>
-                <a href="{{route('todos.show',$todo->id)}}">Read More</a>
+                <a href="{{ route('todos.show',$todo->id) }}">Read More</a>
             </li>
         @empty
             <h4 class="text-center">No Todos Found!</h4>
