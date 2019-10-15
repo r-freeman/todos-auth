c8f81a343e0c704b2512b5cec7135b90c41da2a4
Ryan Freeman <N00173767@student.iadt.ie>
2019-10-15 14:44:21 +0100

    Updated navbar template

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -16,9 +16,6 @@
             <ul class="navbar-nav ml-auto">
                 @auth
                     <li class="nav-item">
-                        <a href="{{ route('todos.index') }}" class="nav-link">Todos</a>
-                    </li>
-                    <li class="nav-item">
                         <a href="{{ route('todos.create') }}" class="nav-link">New Todo</a>
                     </li>
                     <li class="nav-item dropdown">
